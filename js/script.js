d0fcda96fa05cfa1ad2a97bdd1a31b2f5ebd62ed
Monserrat <monserratdiaz12@gmail.com>
2018-02-05 23:20:41 +0000

    started

--- js/script.js
+++ js/script.js
@@ -17,30 +17,30 @@
 
 // Songs
 var mySong = {
-	"title":"24K Magic",
-	"artist":"Bruno Mars",
-	"mp3-url":"https://open.spotify.com/track/6b8Be6ljOzmkOmFslEb23P",
-	"image-url":"https://images-na.ssl-images-amazon.com/images/I/71Gr9aCHQfL._SY355_.jpg",
-}
+	title:"24K Magic",
+	artist:"Bruno Mars",
+	mp3Url:"https://open.spotify.com/track/6b8Be6ljOzmkOmFslEb23P",
+	imageUrl:"https://images-na.ssl-images-amazon.com/images/I/71Gr9aCHQfL._SY355_.jpg",
+};
 
 var myPlayList = [
 	{
-		"title":"24K Magic",
-		"artist":"Bruno Mars",
-		"mp3-url":"https://open.spotify.com/track/6b8Be6ljOzmkOmFslEb23P",
-		"image-url":"https://images-na.ssl-images-amazon.com/images/I/71Gr9aCHQfL._SY355_.jpg",
+		title:"24K Magic",
+		artist:"Bruno Mars",
+		mp3Url:"https://open.spotify.com/track/6b8Be6ljOzmkOmFslEb23P",
+		imageUrl:"https://images-na.ssl-images-amazon.com/images/I/71Gr9aCHQfL._SY355_.jpg",
 	},
 	{
-		"title":"Sir Duke",
-		"artist":"Stevie Wonder",
-		"mp3-url":"https://open.spotify.com/track/2udw7RDkldLFIPG9WYdVtT",
-		"image-url":"https://upload.wikimedia.org/wikipedia/en/thumb/e/e2/Songs_in_the_key_of_life.jpg/220px-Songs_in_the_key_of_life.jpg",
+		title:"Sir Duke",
+		artist:"Stevie Wonder",
+		mp3Url:"https://open.spotify.com/track/2udw7RDkldLFIPG9WYdVtT",
+		imageUrl:"https://upload.wikimedia.org/wikipedia/en/thumb/e/e2/Songs_in_the_key_of_life.jpg/220px-Songs_in_the_key_of_life.jpg",
 	},
 	{
-		"title":"Sorry",
-		"artist":"Justin Bieber",
-		"mp3-url":"https://open.spotify.com/track/09CtPGIpYB4BrO8qb1RGsF",
-		"image-url":"http://assets-s3.usmagazine.com/uploads/assets/articles/93827-justin-biebers-sorry-choreographer-spills-video-style-secrets-parris-goebel/1445638548_justin-bieber-sorry-dancers-zoom.jpg",
+		title:"Sorry",
+		artist:"Justin Bieber",
+		mp3Url:"https://open.spotify.com/track/09CtPGIpYB4BrO8qb1RGsF",
+		imageUrl:"http://assets-s3.usmagazine.com/uploads/assets/articles/93827-justin-biebers-sorry-choreographer-spills-video-style-secrets-parris-goebel/1445638548_justin-bieber-sorry-dancers-zoom.jpg",
 	}
 
 ]
@@ -50,15 +50,23 @@ var myPlayList = [
 // DOCUMENT READY FUNCTION
 $( document ).ready(function() {
   
-
+displayList();
 
 
 });
 
 function displayList(){
-
-
-  
+	$("#addSong").click(function(){
+		for(var i = 0;i<myPlayList.length;i++){
+			$(".songsList").append("<div class = 'row'>" +
+								"<p class='songTitle'>" +myPlayList[i].title + "</p>"+ 
+								"<p class='artist'>" + myPlayList[i].artist + "</p>" + 
+								"<p class='image'><img class='img-thumbnail' src='" + myPlayList[i].imageUrl + "'></p>" +
+								"<p class='url'><a href='" +  myPlayList[i].mp3Url + "'> Listen Now </a></p>"+
+								"</div>"
+								);
+		}
+	});
 }
 
 function clearList(){
