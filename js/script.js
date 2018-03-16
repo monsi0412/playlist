0bf514912be1aaf27fa137b78335aed31c138ea0
Monserrat <monserratdiaz12@gmail.com>
2018-03-16 18:03:55 +0000

    changed background

--- js/script.js
+++ js/script.js
@@ -1,98 +1,13 @@
-// quick fix for Cloud9 warning:
+
 /* global $ */
 
-// Class 2:
-// Complete displayList() to show a single song in the list
 
-// Class 2:
-// Complete displayList() to show all the songs in the list
-// Complete clearList() funcion
 
-// Class 3:
-// Complete the addSong function to take an input from input boxes and push a new song to the playlist array. 
 
-// *************************** YOUR CODE BELOW *******************************
-//******************TEST EARLY AND OFTEN USING console.log() ******************
-//****************** SERIOUSLY TEST USING console.log()!!! ******************
-
-// Songs
-var mySong = {
-	title: "24K Magic",
-	artist: "Bruno Mars",
-	mp3Url: "https://open.spotify.com/track/6b8Be6ljOzmkOmFslEb23P",
-	imageUrl: "https://images-na.ssl-images-amazon.com/images/I/71Gr9aCHQfL._SY355_.jpg",
-};
-
-var myPlayList = [{
-		title: "24K Magic",
-		artist: "Bruno Mars",
-		mp3Url: "https://open.spotify.com/track/6b8Be6ljOzmkOmFslEb23P",
-		imageUrl: "https://images-na.ssl-images-amazon.com/images/I/71Gr9aCHQfL._SY355_.jpg",
-	},
-	{
-		title: "Sir Duke",
-		artist: "Stevie Wonder",
-		mp3Url: "https://open.spotify.com/track/2udw7RDkldLFIPG9WYdVtT",
-		imageUrl: "https://upload.wikimedia.org/wikipedia/en/thumb/e/e2/Songs_in_the_key_of_life.jpg/220px-Songs_in_the_key_of_life.jpg",
-	},
-	{
-		title: "Sorry",
-		artist: "Justin Bieber",
-		mp3Url: "https://open.spotify.com/track/09CtPGIpYB4BrO8qb1RGsF",
-		imageUrl: "http://assets-s3.usmagazine.com/uploads/assets/articles/93827-justin-biebers-sorry-choreographer-spills-video-style-secrets-parris-goebel/1445638548_justin-bieber-sorry-dancers-zoom.jpg",
-	}
-
-];
 
 var newPlaylist = [];
 
 
-// DOCUMENT READY FUNCTION
-$(document).ready(function() {
-	$("#coverArt").keyup(function(event) {
-		if (event.keyCode === 13) {
-			$("#addSong").click();
-		}
-	});
-	$("#songUrl").keyup(function(event) {
-		if (event.keyCode === 13) {
-			$("#addSong").click();
-		}
-	});
-
-	$("#addSong").click(function() {
-		addSong();
-	});
-	// $("#displayPlaylist").click(function() {
-	//   displayNewPlaylist(); 
-	// });
-
-	$(function () {
-  $('[data-toggle="tooltip"]').tooltip();
-});
-
-});
-
-function displayList() {
-	$("#addSong").click(function() {
-		for (var i = 0; i < myPlayList.length; i++) {
-			$(".songsList").append("<div class = 'row'> <div class='col-xs-12'>" +
-				"<p class='songTitle'>" + myPlayList[i].title + "</p>" +
-				"<p class='artist'>" + myPlayList[i].artist + "</p>" +
-				"<p class='image'><img class='img-thumbnail' src='" + myPlayList[i].imageUrl + "'></p>" +
-				"<p class='url'><a target='_blank' href='" + myPlayList[i].mp3Url + "'> Listen Now </a></p>" +
-				"</div></div>"
-			);
-		}
-	});
-}
-
-
-function clearList() {
-
-
-
-}
 
 function addSong() {
 	var songTitle = $("#title").val();
@@ -135,7 +50,10 @@ function addSong() {
 					               <div class='col-xs-5 songDes'>   
 					                 <h3 class='songTitle'>   ${newPlaylist[newPlaylist.length - 1].title }  </h3>   
 					                 <h5 class='artist'>    ${newPlaylist[newPlaylist.length - 1].artist}    </h5>   
-					                 <h7 class='length'>    ${newPlaylist[newPlaylist.length - 1].songLength}    </h7>  
+					                 <h7 class='length'>    ${newPlaylist[newPlaylist.length - 1].songLength}    </h7>
+					                 <p>
+                						<button type='button' class='btn btn-default remove'> Delete Song </button>
+                					</p>
 					               </div>   
 					               <div class='col-xs-1 inBetween'></div>   
 					               <div class='col-xs-5'>   
@@ -155,7 +73,10 @@ function addSong() {
 					               <div class='col-xs-5 songDes'>   
 					                 <h3 class='songTitle'>   ${newPlaylist[newPlaylist.length - 1].title }  </h3>   
 					                 <h5 class='artist'>    ${newPlaylist[newPlaylist.length - 1].artist}    </h5>   
-					                 <h7 class='length'>    ${newPlaylist[newPlaylist.length - 1].songLength}    </h7>  
+					                 <h7 class='length'>    ${newPlaylist[newPlaylist.length - 1].songLength}    </h7> 
+					                 <p>
+                						<button type='button' class='btn btn-default' id='remove'> Delete Song </button>
+                					</p>
 					               </div>   
 					               <div class='col-xs-1 inBetween'></div>   
 					               <div class='col-xs-5'>   
@@ -163,7 +84,7 @@ function addSong() {
 					                   <a target='_blank' href='    ${newPlaylist[newPlaylist.length - 1].url}   
 					                	 '><img class='img-thumbnail' src='    ${newPlaylist[newPlaylist.length - 1].image}    '></a>  
 					                 </p>   
-					               </div>   
+					               </div>
 					             </div>   
 					           </div>   
 					         </div> `);
@@ -171,3 +92,39 @@ function addSong() {
 
 }
 
+function removeSong(){
+	$(".remove").click(function(e) {
+		console.log("this clciked");
+		console.log(e);
+		var songRow= e.currentTarget.parentElement.parentElement.parentElement.parentElement.parentElement;
+		
+	});
+}
+
+// DOCUMENT READY FUNCTION
+$(document).ready(function() {
+	$("#coverArt").keyup(function(event) {
+		if (event.keyCode === 13) {
+			$("#addSong").click();
+		}
+	});
+	$("#songUrl").keyup(function(event) {
+		if (event.keyCode === 13) {
+			$("#addSong").click();
+		}
+	});
+
+	$("#addSong").click(function() {
+		addSong();
+	});
+
+	// $("#delete").click(function(e) {
+	// 	console.log("this clciked");
+	// 	console.log(e);
+	// 	var songRow= e.currentTarget.parentElement.parentElement.parentElement.parentElement.parentElement;
+		
+	// });
+
+	removeSong();
+
+});
